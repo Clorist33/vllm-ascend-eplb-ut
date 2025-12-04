169c4bd36e39ff894eddefcfacaf2c9f9f177b36
tanqingshan (A) <50050625@china.huawei.com>
2025-12-04 21:26:23 +0800

    Feat-4470
    Signed-off-by: tanqingshan (A) <50050625@china.huawei.com>

--- vllm_ascend/ops/fused_moe/moe_mlp.py
+++ vllm_ascend/ops/fused_moe/moe_mlp.py
@@ -127,9 +127,8 @@ def quant_apply_mlp(hidden_states: torch.Tensor,
             if quantized_hidden_states is not None:
                 dispose_tensor(quantized_hidden_states)
             # act_fn: swiglu
-            group_diff = torch.diff(group_list, dim=0)
-            new_group = torch.cat([group_list[0].unsqueeze(0), group_diff],
-                                  dim=0)
+            group_diff = torch.diff(group_list)
+            new_group = torch.cat([group_diff[0].unsqueeze(0), group_diff], dim=0)
             hidden_states, swiglu_out_scale = torch_npu.npu_dequant_swiglu_quant(
                 x=hidden_states,
                 weight_scale=w1_scale,
